f99f67a41c8a336b47a22866ef056f69c75f0bf6
dcwen <dcwen199366@gmail.com>
2022-11-29 13:37:09 +0800

    优化

---
+++ utils/goRuntime/goRuntime_test.go
@@ -0,0 +1,32 @@
+package goRuntime
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGoRun(t *testing.T) {
+	InitGoRuntime()
+	for i := 0; i < 100; i++ {
+		tmp := i
+		GoRun(func(ctx context.Context) {
+			ticker := time.NewTicker(3 * time.Second)
+			defer func() {
+				ticker.Stop()
+				fmt.Println("ticker退出", tmp)
+			}()
+			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("exit", tmp)
+					return
+				case <-ticker.C:
+					fmt.Println("定时")
+				}
+			}
+		})
+	}
+	CloseGoRuntime()
+}
